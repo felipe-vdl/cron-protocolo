988b130a8f8de4361b095b9178adfff709e31f1d
Felipe Vidal <felipe-vdl>
2023-05-25 13:56:54 -0300

    API_KEY

--- src/index.ts
+++ src/index.ts
@@ -10,19 +10,21 @@ declare global {
   }
 }
 
-const timezone = 'America/Sao_Paulo';
+const timezone = "America/Sao_Paulo";
 
-const whatsappJob = new CronJob('*/30 * * * *', async () => {
-  const protocolosNaoEnviados = await prisma.protocolo.findMany({
-    where: {
-      enviar_whatsapp: true,
-      whatsapp_enviado: false
-    }
-  });
+const whatsappJob = new CronJob(
+  "*/30 * * * *",
+  async () => {
+    const protocolosNaoEnviados = await prisma.protocolo.findMany({
+      where: {
+        enviar_whatsapp: true,
+        whatsapp_enviado: false,
+      },
+    });
 
-  if (protocolosNaoEnviados.length > 0) {
-    for (let protocolo of protocolosNaoEnviados) {
-      try {
+    if (protocolosNaoEnviados.length > 0) {
+      for (let protocolo of protocolosNaoEnviados) {
+        try {
           if (protocolo.telefone) {
             const protocoloInfo = {
               inscricao: protocolo.num_inscricao ?? "Não se aplica",
@@ -32,32 +34,36 @@ const whatsappJob = new CronJob('*/30 * * * *', async () => {
               nome: protocolo.nome,
               cpf: protocolo.cpf.replaceAll(".", "").replaceAll("-", ""),
               whatsapp: protocolo.telefone?.replaceAll("-", ""),
-              data: protocolo.created_at.toLocaleDateString("pt-BR")
-            }
-            console.log(protocoloInfo);
-            
+              data: protocolo.created_at.toLocaleDateString("pt-BR"),
+            };
+
             const res = await fetch(process.env.WHATSAPP_API_URL, {
               method: "POST",
               body: JSON.stringify(protocoloInfo),
-            headers: {
-              "Content-Type": "application/json",
-            }
-          });
-
-          if (res.ok) {
-            const updatedProtocolo = await prisma.protocolo.update({
-              where: {
-                id: protocolo.id,
+              headers: {
+                "Content-Type": "application/json",
+                "x-api-key": process.env.WHATSAPP_API_KEY,
               },
-              data: {
-                whatsapp_enviado: true,
-              }
             });
+
+            if (res.ok) {
+              await prisma.protocolo.update({
+                where: {
+                  id: protocolo.id,
+                },
+                data: {
+                  whatsapp_enviado: true,
+                },
+              });
+            }
           }
+        } catch (error) {
+          console.error(error);
         }
-      } catch (error) {
-        console.log(error);
       }
     }
-  }
-}, null, true, timezone);
+  },
+  null,
+  true,
+  timezone
+);
